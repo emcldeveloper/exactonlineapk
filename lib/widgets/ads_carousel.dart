11ad1100cf35b58fea8b8745520288ec17f6d544
johnchuma <johnvchuma@gmail.com>
2025-02-25 13:18:56 +0300

    fix: conversation

--- lib/widgets/ads_carousel.dart
+++ lib/widgets/ads_carousel.dart
@@ -1,5 +1,9 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:carousel_slider/carousel_slider.dart';
+import 'package:e_online/constants/colors.dart';
+import 'package:e_online/controllers/banner_controller.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class AdsCarousel extends StatefulWidget {
   const AdsCarousel({super.key});
@@ -9,35 +13,69 @@ class AdsCarousel extends StatefulWidget {
 }
 
 class _AdsCarouselState extends State<AdsCarousel> {
-  int _currentPage = 0;
-  final List<String> carouselImages = [
-    "assets/ads/ad1.jpg",
-    "assets/ads/ad2.jpg",
-    "assets/ads/ad3.jpg",
-    "assets/ads/ad4.jpg",
-  ];
+  Rx<int> currentPage = Rx<int>(0);
   @override
   Widget build(BuildContext context) {
-    return CarouselSlider(
-      options: CarouselOptions(
-        autoPlay: true,
-        autoPlayInterval: const Duration(seconds: 4),
-        initialPage: 0,
-        height: 160,
-        viewportFraction: 1,
-        onPageChanged: (value, _) {
-          setState(() {
-            _currentPage = value;
-          });
-        },
-      ),
-      items: carouselImages.map((i) {
-        return Builder(
-          builder: (BuildContext context) {
-            return Image.asset(i, fit: BoxFit.contain);
-          },
-        );
-      }).toList(),
-    );
+    return FutureBuilder(
+        future: BannersController().getBanners(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Container();
+          }
+          List<dynamic> banners =
+              snapshot.requireData.map((item) => item["image"]).toList();
+          return Column(
+            children: [
+              CarouselSlider(
+                options: CarouselOptions(
+                  autoPlay: true,
+                  autoPlayInterval: const Duration(seconds: 4),
+                  height: 160,
+                  viewportFraction: 1,
+                  onPageChanged: (value, _) {
+                    currentPage.value = value;
+                  },
+                ),
+                items: banners.map((i) {
+                  return Builder(
+                    builder: (BuildContext context) {
+                      return Container(
+                        width: double.infinity,
+                        child: CachedNetworkImage(
+                          imageUrl: i,
+                          fit: BoxFit.cover,
+                        ),
+                      );
+                    },
+                  );
+                }).toList(),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children:
+                      List.generate(banners.length, (int number) => number++)
+                          .map((i) {
+                    return Padding(
+                      padding: const EdgeInsets.all(5.0),
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(50),
+                        child: Obx(
+                          () => Container(
+                            height: 7,
+                            width: i == currentPage.value ? 15 : 7,
+                            color: i == currentPage.value
+                                ? secondaryColor
+                                : const Color(0xffEBEBEB),
+                          ),
+                        ),
+                      ),
+                    );
+                  }).toList())
+            ],
+          );
+        });
   }
 }
